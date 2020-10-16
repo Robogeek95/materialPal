e46439d3b97ee4ef6757c6101b556a215eb70d2c
Azeez Lukman <azeezlukman95@gmail.com>
2020-10-16 09:26:34 +0100

    updated design

--- pages/search.js
+++ pages/search.js
@@ -11,6 +11,22 @@ import {
   Text,
 } from "theme-ui";
 
+import { Icon, InlineIcon } from "@iconify/react";
+import filterIcon from "@iconify/icons-mi/filter";
+
+const categories = [
+  "Assessment",
+  "Note",
+  "Test",
+  "Syllabus",
+  "Lessons",
+  "Reports",
+  "Assignments",
+  "Past Questions",
+  "Text Books",
+  "Handouts",
+];
+
 const materials = [
   {
     name: "CSC 111 past questions ",
@@ -70,63 +86,140 @@ const materials = [
 
 const Search = () => (
   <>
-    <Nav />
+    {/* Search Bar */}
 
-    {/* Header */}
     <Box
       as="header"
       sx={{
         bg: "snow",
-        pt: [5, 6],
-        pb: [4, 5],
+        pt: [3, 3],
+        pb: [3, 3],
       }}
     >
       <Container>
         {/* sitemap */}
-        <Text as="p" variant="subtitle">
-          Search{">"} course
+        <Text
+          as="p"
+          variant="subtitle"
+          sx={{
+            textShadow: " 0px 4px 4px rgba(0, 0, 0, 0.25)",
+            fontFamily: "Princess Sofia",
+            fontSize: "26px",
+          }}
+        >
+          Material Pal
         </Text>
 
-        <Input defaultValue="CSC 111" />
-        {/* <Text>CSC 111</Text> */}
+        <Grid columns={["8fr 4fr"]}>
+          <Box>
+            {/* search bar */}
+            <Box>
+              <Input defaultValue="CSC 111" />
+            </Box>
 
-        {/* filter */}
-        <Box as="form" onSubmit={(e) => e.preventDefault()}>
-          <Grid columns="auto 1fr 1fr 1fr" gap={[3, 4]} pt={[3, 4]}>
+            {/* categories */}
             <Box
+              mt={[4]}
               sx={{
-                display: "flex",
-                alignItems: "center",
+                color: "#000000",
+                textTransform: "uppercase",
+                overflow: "hidden",
+                whiteSpace: "nowrap",
               }}
             >
-              <Text as="p">Filter by</Text>
+              <Text
+                as="a"
+                mr={[3]}
+                sx={{
+                  color: "#247B87",
+                  display: "inline-block",
+                  opacity: "1",
+                }}
+              >
+                All
+              </Text>
+              {categories.map((category) => (
+                <Text
+                  as="a"
+                  mr={[3]}
+                  sx={{
+                    display: "inline-block",
+                    opacity: "0.3",
+                  }}
+                >
+                  {category}
+                </Text>
+              ))}
             </Box>
+          </Box>
+        </Grid>
 
-            <Box as="div">
-              <Label htmlFor="username">School</Label>
-              <Input defaultValue="School" />
-            </Box>
-            <Box as="div">
-              <Label htmlFor="username">Department</Label>
-              <Input defaultValue="Department" />
-            </Box>
-            <Box as="div">
-              <Label htmlFor="username">Course</Label>
-              <Input defaultValue="Course" />
-            </Box>
-          </Grid>
-        </Box>
+        {/* <Text>CSC 111</Text> */}
       </Container>
     </Box>
 
     <Box>
       <Container>
-        <Grid columns={[null, "8fr 2fr"]}>
+        {/* filter */}
+        <Grid columns={["8fr 4fr"]}>
+          <Box as="form" onSubmit={(e) => e.preventDefault()}>
+            <Grid
+              columns="auto 1fr 1fr 1fr auto"
+              gap={[3, 4]}
+              pt={[3, 4]}
+              sx={{
+                input: {
+                  background: "#FAFAFA",
+                  border: "1px solid #DCDCDC",
+                  boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+                  borderRadius: "5px",
+                },
+              }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                <Text as="p">Filter by</Text>
+              </Box>
+
+              <Box as="div">
+                <Input defaultValue="School" />
+              </Box>
+              <Box as="div">
+                <Input defaultValue="Department" />
+              </Box>
+              <Box as="div">
+                <Input defaultValue="Course" />
+              </Box>
+
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                <Icon
+                  icon={filterIcon}
+                  style={{ color: "#01333f", fontSize: "24px" }}
+                />
+              </Box>
+            </Grid>
+          </Box>
+        </Grid>
+      </Container>
+    </Box>
+
+    <Box>
+      <Container>
+        <Grid columns={[null, "8fr 4fr"]}>
           <Box>
             <Grid
               my={[4]}
               gap={[4, 4]}
-              columns={[null, 5]}
+              columns={[null, 4]}
               sx={{
                 textAlign: "left",
                 "> a, > div": {
@@ -154,11 +247,16 @@ const Search = () => (
                     paddingBottom: [4],
                   }}
                 >
-                  <Image src="/search/book.png" />
-                  <Text as="P">{material.name}</Text>
-                  <Text as="P">by {material.author}</Text>
-                  <Text as="P">rating {material.rating}</Text>
-                  <Text as="P">pages {material.pages}</Text>
+                  <Image
+                    src="/search/book.png"
+                    sx={{ width: "100%", height: "175px" }}
+                  />
+                  <Box sx={{ padding: "16px" }}>
+                    <Text as="P">{material.name}</Text>
+                    <Text as="P">by {material.author}</Text>
+                    <Text as="P">rating {material.rating}</Text>
+                    <Text as="P">pages {material.pages}</Text>
+                  </Box>
                 </Card>
               ))}
             </Grid>
