6d968307392e9cc23095d15decc5ea7616eb9a84
Azeez Lukman <azeezlukman95@gmail.com>
2020-10-20 12:08:39 +0100

    added institution

--- pages/search.js
+++ pages/search.js
@@ -15,6 +15,7 @@ import {
 import { Icon, InlineIcon } from "@iconify/react";
 import filterIcon from "@iconify/icons-mi/filter";
 import baselineSearch from "@iconify/icons-ic/baseline-search";
+import Link from "next/link";
 
 const categories = [
   "Assessment",
@@ -192,7 +193,7 @@ const Search = () => (
         <Grid columns={["1fr", null, null, "8fr 4fr"]}>
           <Box as="form" onSubmit={(e) => e.preventDefault()}>
             <Grid
-              columns="1fr 1fr 1fr auto"
+              columns="1fr 1fr auto"
               gap={[3, 4]}
               pt={[3, 4]}
               sx={{
@@ -214,10 +215,7 @@ const Search = () => (
               <Box as="div">
                 <Input placeholder="Department" />
               </Box>
-              <Box as="div">
-                <Input placeholder="Course" />
-              </Box>
-
+             
               <IconButton
                 sx={{
                   display: "flex",
@@ -245,11 +243,10 @@ const Search = () => (
 
     <Box>
       <Container>
-        <Grid columns={["1fr", null, null, "8fr 4fr"]}>
+        <Grid columns={["1fr", null, null, "8fr 4fr"]} gap={[4]}>
           <Box>
             <Grid
               my={[4]}
-              gap={[3, 4]}
               columns={[2, 4]}
               sx={{
                 textAlign: "left",
@@ -283,10 +280,14 @@ const Search = () => (
                     sx={{ width: "100%", height: "175px" }}
                   />
                   <Box sx={{ padding: "16px" }}>
-                    <Text as="P" variant="subheadline" sx={{ }}>
+                    <Text as="P" variant="subheadline" mb={[0]} sx={{}}>
                       {material.name}
                     </Text>
-                    <Text as="P" variant="lead" sx={{ color: "cyan"}}>
+                    <Text
+                      as="P"
+                      my={[2]}
+                      sx={{ color: "cyan", fontSize: "16px" }}
+                    >
                       {material.author}
                     </Text>
                     <Grid columns={["1fr 1fr"]}>
@@ -303,13 +304,66 @@ const Search = () => (
             </Grid>
           </Box>
 
-          <Box
-            sx={
-              {
-                //   background: "green",
-              }
-            }
-          ></Box>
+          <Box mt={[4]}>
+            <Card variant="detailCard">
+              <Grid columns={["auto 1fr"]}>
+                <Box
+                  sx={{
+                    width: "100px",
+                    height: "150px",
+                  }}
+                >
+                  <Image src="/search/lasu_logo.png" />
+                </Box>
+                <Box>
+                  <Text variant="headline" m={[0]}>
+                    Lagos State University
+                  </Text>
+                  <Text variant="subtitle">Lagos, Nigeria</Text>
+                  <Link href="https://lasu.edu.ng/">www.lasu.edu.ng</Link>
+                </Box>
+              </Grid>
+              <Box mt={[3]}>
+                <Text sx={{ color: "steel" }}>Departments</Text>
+                <Text
+                  sx={{
+                    color: "darkBlue",
+                    fontWeight: "bold",
+                    fontSize: 2,
+                    my: 1,
+                  }}
+                >
+                  26
+                </Text>
+
+<hr />
+
+                <Text sx={{ color: "steel" }}>Courses</Text>
+                <Text
+                  sx={{
+                    color: "darkBlue",
+                    fontWeight: "bold",
+                    fontSize: 2,
+                    my: 1,
+                  }}
+                >
+                  30
+                </Text>
+
+                <Text>Materials on material pal</Text>
+                <Text
+                  sx={{
+                    color: "darkBlue",
+                    fontWeight: "bold",
+                    fontSize: 2,
+                    my: 1,
+                  }}
+                >
+                  341
+                </Text>
+              </Box>
+            </Card>
+          </Box>
         </Grid>
       </Container>
     </Box>
