9e364a51e77db505f0a973d794517dda0190b0b5
Azeez Lukman <azeezlukman95@gmail.com>
2020-11-25 19:45:43 +0100

    themed search page

--- pages/materials/index.js
+++ pages/materials/index.js
@@ -4,6 +4,7 @@ import {
   Button,
   Card,
   Container,
+  Flex,
   Grid,
   IconButton,
   Image,
@@ -17,81 +18,197 @@ import Link from "next/link";
 import materials from "../../lib/materials.json";
 import SearchBar from "../../components/SearchBar";
 import MaterialModal from "../../components/MaterialModal";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Nav from "../../components/nav";
+import {
+  faBaseballBall,
+  faBasketballBall,
+  faBook,
+  faFilter,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Search = () => (
   <>
-    {/* Search Bar */}
-    <SearchBar />
+    <Nav />
 
-    <Box>
-      <Container>
-        {/* filter */}
-        <MaterialModal />
-
-        <Grid columns={["1fr", null, null, "8fr 4fr"]}>
-          <Box as="form" onSubmit={(e) => e.preventDefault()}>
-            <Grid
-              columns="1fr 1fr auto"
-              gap={[3, 4]}
-              pt={[3, 4]}
-              sx={{
-                ".input_container": {
-                  background: "#FFFFFF",
-                  boxShadow: "small",
-                  borderRadius: "default",
-                  border: "1px solid black",
-                  ":hover": {
-                    boxShadow: "elevated",
-                  },
-                  px: [3],
+    <Container mt={[5]}>
+      <Grid columns={["1fr", null, null, "8fr 4fr"]}>
+        <Box as="form" onSubmit={(e) => e.preventDefault()}>
+          <Grid
+            columns="1fr 1fr 1fr auto"
+            gap={[3, 4]}
+            pt={[3, 4]}
+            sx={{
+              ".input_container": {
+                background: "#FFFFFF",
+                boxShadow: "small",
+                borderRadius: "default",
+                border: "1px solid black",
+                ":hover": {
+                  boxShadow: "elevated",
                 },
+                px: [3],
+              },
+            }}
+          >
+            <Box>
+              <Select defaultValue="Hello">
+                <option>School</option>
+                <option>Hi</option>
+                <option>Beep</option>
+                <option>Boop</option>
+              </Select>
+            </Box>
+
+            <Box>
+              <Select defaultValue="Hello">
+                <option>Department</option>
+                <option>Hi</option>
+                <option>Beep</option>
+                <option>Boop</option>
+              </Select>
+            </Box>
+
+            <Box>
+              <Select defaultValue="Hello">
+                <option>Course</option>
+                <option>Hi</option>
+                <option>Beep</option>
+                <option>Boop</option>
+              </Select>
+            </Box>
+
+            <IconButton
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                width: "35px",
+                height: "35px",
+                border: "none",
+                backgroundColor: "lighter",
+                borderRadius: "default",
+                boxShadow: "card",
+                color: "gray200",
+                // mt: ["25px"],
+                cursor: "pointer",
               }}
             >
-              <Box>
-                <Label htmlFor="school" sx={{ color: "muted" }}>
-                  <Text variant="small">School</Text>
-                </Label>
-                <Box className="input_container">
-                  <Select name="school" placeholder="Enter School">
-                    <option>Lagos State University</option>
-                  </Select>
-                </Box>
-              </Box>
+              <FontAwesomeIcon icon={faFilter} />
+            </IconButton>
+          </Grid>
 
-              <Box>
-                <Label htmlFor="school" sx={{ color: "muted" }}>
-                  <Text variant="small">Deartment</Text>
-                </Label>
-                <Box className="input_container">
-                  <Select name="department" placeholder="Enter Department" />
-                </Box>
-              </Box>
+          <Box></Box>
+        </Box>
+      </Grid>
 
-              <IconButton
+      <Grid columns={["1fr", null, null, "8fr 4fr"]} my={[4]}>
+        <Grid columns={[2, 4]}>
+          {materials.map((material) => (
+            <Card key={material.id} variant="detailCard" pb="2">
+              <Box>
+                <Image
+                  src="./search/book.png"
+                  width="100%"
+                  height="auto"
+                ></Image>
+              </Box>
+              <Box p="2">
+                <Flex sx={{ minHeight: "50px", alignItems: "center" }}>
+                  <Text variant="lead">{material.name}</Text>
+                </Flex>
+
+                <Flex>
+                  {material.tags.map((tag) => (
+                    <Text variant="smallLabel" color="dark300">
+                      {tag}
+                    </Text>
+                  ))}
+                </Flex>
+
+                <Text variant="mediumLabel" color="darker">
+                  {material.author}
+                </Text>
+
+                <Flex
+                  sx={{
+                    mt: "2",
+                    color: "darker",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <Flex>
+                    <FontAwesomeIcon icon={faBasketballBall}></FontAwesomeIcon>
+                    <Text ml="2" variant="smallText">
+                      {material.rating} Stars
+                    </Text>
+                  </Flex>
+
+                  <Flex>
+                    <FontAwesomeIcon icon={faBook}></FontAwesomeIcon>
+                    <Text ml="2" variant="smallText">
+                      {material.pages} pages
+                    </Text>
+                  </Flex>
+                </Flex>
+              </Box>
+            </Card>
+          ))}
+        </Grid>
+
+        <Box sx={{ display: ["none", null, "block"] }}>
+          <Card variant="detailCard" p={[4]}>
+            <Grid columns={["auto 1fr"]}>
+              <Box
                 sx={{
-                  display: "flex",
-                  alignItems: "center",
-                  width: "35px",
-                  height: "35px",
-                  border: "none",
-                  background: "#FFFFFF",
-                  borderRadius: "default",
-                  boxShadow: "card",
-                  mt: ["25px"],
-                  cursor: "pointer",
+                  width: "100px",
+                  height: "150px",
                 }}
               >
-                
-               </IconButton>
+                <Image src="/search/lasu_logo.png" />
+              </Box>
+              <Box>
+                <Text variant="headline4" color="darker" m={[0]}>
+                  Lagos State University
+                </Text>
+                <Text variant="largeaLabel">Lagos, Nigeria</Text>
+                <Text variant="body" color="lighter">
+                  <Link href="https://lasu.edu.ng/" target="blank">
+                    www.lasu.edu.ng
+                  </Link>
+                </Text>
+              </Box>
             </Grid>
+            <Box mt={[3]}>
+              <Text variant="mediumLabel" color="dark200">
+                Departments
+              </Text>
+              <Text variant="headline6" color="darker">
+                26
+              </Text>
+              <hr />
 
-            <Box></Box>
-          </Box>
-        </Grid>
-      </Container>
-    </Box>
+              <Text variant="mediumLabel" color="dark200">
+                Courses
+              </Text>
+              <Text variant="headline6" color="darker">
+                30
+              </Text>
 
-    <Box>
+              <hr />
+
+              <Text variant="mediumLabel" color="dark200">
+                Materials on material pal
+              </Text>
+              <Text variant="headline6" color="darker">
+                341
+              </Text>
+            </Box>
+          </Card>
+        </Box>
+      </Grid>
+    </Container>
+
+    {/* <Box>
       <Container>
         <Grid columns={["1fr", null, null, "8fr 4fr"]} gap={[4]}>
           <Box>
@@ -217,7 +334,7 @@ const Search = () => (
           </Box>
         </Grid>
       </Container>
-    </Box>
+    </Box> */}
 
     <Footer
       dark
