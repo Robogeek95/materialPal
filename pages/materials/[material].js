c37ca083f261bdc61feff0f656c65014203482d2
Azeez Lukman <azeezlukman95@gmail.com>
2020-12-04 21:41:36 +0100

    adding material page

--- pages/materials/[material].js
+++ pages/materials/[material].js
@@ -1,59 +1,158 @@
+import { faStar, faTenge } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRouter } from "next/router";
-import { Box, Container, Grid, Image, Text } from "theme-ui";
+import { Box, Container, Flex, Grid, Image, Text } from "theme-ui";
 import Nav from "../../components/nav";
 import materials from "../../lib/materials.json";
+import mymaterials from "../../lib/materials.json";
 
-const Material = () => {
-  const router = useRouter();
-  const { material } = router.query;
-  console.log(material);
-  let selectedMaterial = [];
-
-  if (material) {
-    console.log(material);
-
-    selectedMaterial = materials.filter((mat) =>
-      console.log(mat.id === material)
-    );
-  }
-
-  console.log(material);
-  console.log(selectedMaterial);
+const materialPage = (props) => {
+  let material = props.material[0];
   return (
     <>
       <Nav />
-      <Box as="section" sx={{ pt: [5, 6], pb: [4, 5] }}>
-        <Container>
-          {/* filter */}
-          <Grid columns={["1fr", null, null, "8fr 4fr"]}>
-            <Grid columns={["1fr", null, null, "4fr 8fr"]} sx={{ bg: "snow" }}>
-              <Box>
-                <Image src="/search/book.png" height="100%" width="100%" />
-              </Box>
 
-              <Box>
-                <Text variant="subtitle">{selectedMaterial.name}</Text>
-                <Text as="P" my={[2]} sx={{ color: "cyan", fontSize: "16px" }}>
-                  {selectedMaterial.author}
+      <Container>
+        <Grid
+          as="section"
+          columns={"10fr 2fr"}
+          mt={[6]}
+          p="2"
+          sx={{ boxShadow: "card" }}
+        >
+          <Grid columns={[2]}>
+            <Box>
+              <Image
+                height="auto"
+                width="100%"
+                src="/search/austrian-national-library.jpg"
+              />
+            </Box>
+
+            <Box>
+              {/* filter */}
+              <Text variant="headline4"> {material.name} </Text>
+              <Text variant="body">
+                uploaded by{" "}
+                <Text as="span" variant="headline6">
+                  {material.author}
                 </Text>
+              </Text>
+              <Text variant="body" mt="2">
+                {material.description}
+              </Text>
+              {material.name}
 
-                <hr />
+              <hr />
 
-                <Grid columns="auto auto auto auto">
-                  <Text>{selectedMaterial.rating}</Text>
-                  <Text>{selectedMaterial.pages}</Text>
-                  <Text>{selectedMaterial.pages}</Text>
-                  <Text>{selectedMaterial.pages}</Text>
+              <Box>
+                <Flex>
+                  <FontAwesomeIcon
+                    sx={{ alignItems: "center" }}
+                    icon={faTenge}
+                  />
+                  <Text ml={2} variant="headline6">
+                    Hide Details
+                  </Text>
+                </Flex>
+
+                <Grid columns={["auto auto auto"]}>
+                  <Flex mt={3} sx={{ alignItems: "center" }}>
+                    <FontAwesomeIcon icon={faStar} />
+                    <Text ml={2} variant="smallText">
+                      {material.details.rating} stars
+                    </Text>
+                  </Flex>
+
+                  <Flex mt={3} sx={{ alignItems: "center" }}>
+                    <FontAwesomeIcon icon={faStar} />
+                    <Text ml={2} variant="smallText">
+                      {material.details.pages} pages
+                    </Text>
+                  </Flex>
+
+                  <Flex mt={3} sx={{ alignItems: "center" }}>
+                    <FontAwesomeIcon icon={faStar} />
+                    <Text ml={2} variant="smallText">
+                      {material.details.downloads} downloads
+                    </Text>
+                  </Flex>
+
+                  <Flex mt={3} sx={{ alignItems: "center" }}>
+                    <FontAwesomeIcon icon={faStar} />
+                    <Text ml={2} variant="smallText">
+                      {material.details.tags.map((tag) => (
+                        <Text variant="smallLabel" color="dark300">
+                          {tag}
+                        </Text>
+                      ))}
+                    </Text>
+                  </Flex>
+
+                  <Flex mt={3} sx={{ alignItems: "center" }}>
+                    <FontAwesomeIcon icon={faStar} />
+                    <Text ml={2} variant="smallText">
+                      {material.details.date}
+                    </Text>
+                  </Flex>
+
+                  <Flex mt={3} sx={{ alignItems: "center" }}>
+                    <FontAwesomeIcon icon={faStar} />
+                    <Text ml={2} variant="smallText">
+                      {material.details.school}
+                    </Text>
+                  </Flex>
+
+                  <Flex mt={3} sx={{ alignItems: "center" }}>
+                    <FontAwesomeIcon icon={faStar} />
+                    <Text ml={2} variant="smallText">
+                      {material.details.course}
+                    </Text>
+                  </Flex>
                 </Grid>
-
-                <Box></Box>
               </Box>
-            </Grid>
+
+              <hr />
+
+              <Box>
+                <Box>
+                  <Image src="/search/book.png" />
+                </Box>
+              </Box>
+            </Box>
           </Grid>
-        </Container>
-      </Box>
+        </Grid>
+      </Container>
     </>
   );
 };
 
-export default Material;
+// This function gets called at build time
+export async function getStaticPaths() {
+  // Call an external API endpoint to get posts
+  const materials = mymaterials;
+
+  // Get the paths we want to pre-render based on posts
+  const paths = materials.map((material) => `/materials/${material.id}`);
+
+  // We'll pre-render only these paths at build time.
+  // { fallback: false } means other routes should 404.
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params }) {
+  // params contains the post `id`.
+  // If the route is like /posts/1, then params.id is 1
+  // const res = await fetch(`https://.../posts/${params.id}`);
+  const material = materials.filter(
+    (material) => material.id.toString() === params.material
+  );
+
+  console.log(material);
+  // const post = await res.json();
+
+  // Pass post data to the page via props
+  return { props: { material } };
+}
+
+export default materialPage;
--- pages/materials/index.js
+++ pages/materials/index.js
@@ -15,8 +15,8 @@ import {
 } from "theme-ui";
 
 import Link from "next/link";
-import materials from "../../lib/materials.json";
-import categories from "../../lib/categories.json";
+import myMaterials from "../../lib/materials.json";
+import myCategories from "../../lib/categories.json";
 import SearchBar from "../../components/SearchBar";
 import MaterialModal from "../../components/MaterialModal";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -28,292 +28,183 @@ import {
   faFilter,
 } from "@fortawesome/free-solid-svg-icons";
 
-const Search = () => (
-  <>
-    <Nav />
+const Materials = ({ materials, categories }) => {
+  return (
+    <>
+      <Nav />
 
-    <Box bg="gray300" pt={[4]} pb={[0]}>
-      <Container pt={[4, 5]}>
-        <Grid columns={["1fr", null, null, "8fr 4fr"]}>
-          <Box
-            sx={{
-              display: "flex",
-              flexWrap: "nowrap",
-              overflowX: "auto",  
-              py: [4],
-            }}
-          >
-            {categories.map((category) => (
-              <Button
-                variant="outline"
-                as="a"
-                mr={[3]}
-                sx={{
-                  display: "inline-block",
-                  opacity: "0.3",
-                  flex: "0 0 auto",
-                }}
-              >
-                {category}
-              </Button>
-            ))}
-          </Box>
-        </Grid>
-      </Container>
-    </Box>
-
-    <Container>
-      <Box as="form" onSubmit={(e) => e.preventDefault()}>
-        <Grid
-          columns="1fr auto"
-          gap={[3, 4]}
-          pt={[3, 4]}
-          sx={{
-            ".input_container": {
-              background: "#FFFFFF",
-              boxShadow: "small",
-              borderRadius: "default",
-              border: "1px solid black",
-              ":hover": {
-                boxShadow: "elevated",
-              },
-              px: [3],
-            },
-          }}
-        >
-          <Box
-            sx={{
-              display: "flex",
-              flexWrap: "nowrap",
-              overflowX: "auto",
-              div: {
-                width: "220px",
-                pr: "3",
-                flex: "0 0 auto",
-              },
-            }}
-          >
-            <Box>
-              <Select defaultValue="Hello">
-                <option>School</option>
-                <option>Hi</option>
-                <option>Beep</option>
-                <option>Boop</option>
-              </Select>
-            </Box>
-
-            <Box>
-              <Select defaultValue="Hello">
-                <option>Department</option>
-                <option>Hi</option>
-                <option>Beep</option>
-                <option>Boop</option>
-              </Select>
-            </Box>
-
-            <Box>
-              <Select defaultValue="Hello">
-                <option>Course</option>
-                <option>Hi</option>
-                <option>Beep</option>
-                <option>Boop</option>
-              </Select>
-            </Box>
-          </Box>
-
-          <IconButton
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              width: "35px",
-              height: "35px",
-              border: "none",
-              backgroundColor: "lighter",
-              borderRadius: "default",
-              boxShadow: "card",
-              color: "gray200",
-              // mt: ["25px"],
-              cursor: "pointer",
-            }}
-          >
-            <FontAwesomeIcon icon={faFilter} />
-          </IconButton>
-        </Grid>
-
-        <Box></Box>
-      </Box>
-
-      <Grid columns={["1fr", null, null, "8fr 4fr"]} my={[4]}>
-        <Grid columns={[2, 4]}>
-          {materials.map((material) => (
-            <Card key={material.id} variant="detailCard" pb="2">
-              <Box>
-                <Image
-                  src="./search/book.png"
-                  width="100%"
-                  height="auto"
-                ></Image>
-              </Box>
-              <Box p="2">
-                <Flex sx={{ minHeight: "50px", alignItems: "center" }}>
-                  <Text variant="lead">{material.name}</Text>
-                </Flex>
-
-                <Flex>
-                  {material.tags.map((tag) => (
-                    <Text variant="smallLabel" color="dark300">
-                      {tag}
-                    </Text>
-                  ))}
-                </Flex>
-
-                <Text variant="mediumLabel" color="darker">
-                  {material.author}
-                </Text>
-
-                <Flex
-                  sx={{
-                    mt: "2",
-                    color: "darker",
-                    justifyContent: "space-between",
-                  }}
-                >
-                  <Flex>
-                    <FontAwesomeIcon icon={faBasketballBall}></FontAwesomeIcon>
-                    <Text ml="2" variant="smallText">
-                      {material.rating} Stars
-                    </Text>
-                  </Flex>
-
-                  <Flex>
-                    <FontAwesomeIcon icon={faBook}></FontAwesomeIcon>
-                    <Text ml="2" variant="smallText">
-                      {material.pages} pages
-                    </Text>
-                  </Flex>
-                </Flex>
-              </Box>
-            </Card>
-          ))}
-        </Grid>
-
-        <Box sx={{ display: ["none", null, "block"] }}>
-          <Card variant="detailCard" p={[4]}>
-            <Grid columns={["auto 1fr"]}>
-              <Box
-                sx={{
-                  width: "100px",
-                  height: "150px",
-                }}
-              >
-                <Image src="/search/lasu_logo.png" />
-              </Box>
-              <Box>
-                <Text variant="headline4" color="darker" m={[0]}>
-                  Lagos State University
-                </Text>
-                <Text variant="largeaLabel">Lagos, Nigeria</Text>
-                <Text variant="body" color="lighter">
-                  <Link href="https://lasu.edu.ng/" target="blank">
-                    www.lasu.edu.ng
-                  </Link>
-                </Text>
-              </Box>
-            </Grid>
-            <Box mt={[3]}>
-              <Text variant="mediumLabel" color="dark200">
-                Departments
-              </Text>
-              <Text variant="headline6" color="darker">
-                26
-              </Text>
-              <hr />
-
-              <Text variant="mediumLabel" color="dark200">
-                Courses
-              </Text>
-              <Text variant="headline6" color="darker">
-                30
-              </Text>
-
-              <hr />
-
-              <Text variant="mediumLabel" color="dark200">
-                Materials on material pal
-              </Text>
-              <Text variant="headline6" color="darker">
-                341
-              </Text>
-            </Box>
-          </Card>
-        </Box>
-      </Grid>
-    </Container>
-
-    {/* <Box>
-      <Container>
-        <Grid columns={["1fr", null, null, "8fr 4fr"]} gap={[4]}>
-          <Box>
-            <Grid
-              my={[4]}
-              columns={[2, 4]}
+      <Box bg="gray300" pt={[4]} pb={[0]}>
+        <Container pt={[4, 5]}>
+          <Grid columns={["1fr", null, null, "8fr 4fr"]}>
+            <Box
               sx={{
-                textAlign: "left",
-                "> a, > div": {
-                  borderRadius: "extra",
-                  boxShadow: "elevated",
-                  px: [3, null, 4],
-                  py: [4, null, 5],
-                },
-                span: {
-                  boxShadow:
-                    "-2px -2px 6px rgba(255,255,255,0.125), inset 2px 2px 6px rgba(0,0,0,0.1), 2px 2px 8px rgba(0,0,0,0.0625)",
-                },
-                svg: { fill: "currentColor" },
+                display: "flex",
+                flexWrap: "nowrap",
+                overflowX: "auto",
+                py: [4],
               }}
             >
-              {materials.map((material) => (
-                <Card
+              {categories.map((category) => (
+                <Button
+                  variant="outline"
                   as="a"
-                  href={`/materials/${material.id}`}
-                  variant="interactive"
+                  mr={[3]}
                   sx={{
-                    background: "#1b4758",
-                    color: "#f7f2f6",
-                    fontSize: "x-large",
-                    cursor: "pointer",
-                    padding: "0 !important",
-                    paddingBottom: [4],
+                    display: "inline-block",
+                    opacity: "0.3",
+                    flex: "0 0 auto",
                   }}
                 >
-                  <Image
-                    src="/search/book.png"
-                    sx={{ width: "100%", height: "175px" }}
-                  />
-                  <Box sx={{ padding: "16px" }}>
-                    <Text as="P" variant="subheadline" mb={[0]} sx={{}}>
-                      {material.name}
-                    </Text>
-                    <Text
-                      as="P"
-                      my={[2]}
-                      sx={{ color: "cyan", fontSize: "16px" }}
-                    >
+                  {category}
+                </Button>
+              ))}
+            </Box>
+          </Grid>
+        </Container>
+      </Box>
+
+      <Container>
+        <Box as="form" onSubmit={(e) => e.preventDefault()}>
+          <Grid
+            columns="1fr auto"
+            gap={[3, 4]}
+            pt={[3, 4]}
+            sx={{
+              ".input_container": {
+                background: "#FFFFFF",
+                boxShadow: "small",
+                borderRadius: "default",
+                border: "1px solid black",
+                ":hover": {
+                  boxShadow: "elevated",
+                },
+                px: [3],
+              },
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                flexWrap: "nowrap",
+                overflowX: "auto",
+                div: {
+                  width: "220px",
+                  pr: "3",
+                  flex: "0 0 auto",
+                },
+              }}
+            >
+              <Box>
+                <Select defaultValue="Hello">
+                  <option>School</option>
+                  <option>Hi</option>
+                  <option>Beep</option>
+                  <option>Boop</option>
+                </Select>
+              </Box>
+
+              <Box>
+                <Select defaultValue="Hello">
+                  <option>Department</option>
+                  <option>Hi</option>
+                  <option>Beep</option>
+                  <option>Boop</option>
+                </Select>
+              </Box>
+
+              <Box>
+                <Select defaultValue="Hello">
+                  <option>Course</option>
+                  <option>Hi</option>
+                  <option>Beep</option>
+                  <option>Boop</option>
+                </Select>
+              </Box>
+            </Box>
+
+            <IconButton
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                width: "35px",
+                height: "35px",
+                border: "none",
+                backgroundColor: "lighter",
+                borderRadius: "default",
+                boxShadow: "card",
+                color: "gray200",
+                // mt: ["25px"],
+                cursor: "pointer",
+              }}
+            >
+              <FontAwesomeIcon icon={faFilter} />
+            </IconButton>
+          </Grid>
+
+          <Box></Box>
+        </Box>
+
+        <Grid columns={["1fr", null, null, "8fr 4fr"]} my={[4]}>
+          <Grid columns={[2, 4]}>
+            {materials.map((material) => (
+              <Link key={material.id} href={`./materials/${material.id}`}>
+                <Card key={material.id} variant="detailCard" pb="2">
+                  <Box>
+                    <Image
+                      src="./search/book.png"
+                      alt={`cover photo for ${material.name}`}
+                      width="100%"
+                      height="auto"
+                    ></Image>
+                  </Box>
+                  <Box p="2">
+                    <Flex sx={{ minHeight: "50px", alignItems: "center" }}>
+                      <Text variant="lead">{material.name}</Text>
+                    </Flex>
+
+                    <Flex>
+                      {material.details.tags.map((tag) => (
+                        <Text variant="smallLabel" color="dark300">
+                          {tag}
+                        </Text>
+                      ))}
+                    </Flex>
+
+                    <Text variant="mediumLabel" color="darker">
                       {material.author}
                     </Text>
-                    <Grid columns={[]}>
-                      <Text variant="small" as="P">
-                        {material.pages} pages
-                      </Text>
-                    </Grid>
+
+                    <Flex
+                      sx={{
+                        mt: "2",
+                        color: "darker",
+                        justifyContent: "space-between",
+                      }}
+                    >
+                      <Flex>
+                        <FontAwesomeIcon
+                          icon={faBasketballBall}
+                        ></FontAwesomeIcon>
+                        <Text ml="2" variant="smallText">
+                          {material.details.rating} Stars
+                        </Text>
+                      </Flex>
+
+                      <Flex>
+                        <FontAwesomeIcon icon={faBook}></FontAwesomeIcon>
+                        <Text ml="2" variant="smallText">
+                          {material.details.pages} pages
+                        </Text>
+                      </Flex>
+                    </Flex>
                   </Box>
                 </Card>
-              ))}
-            </Grid>
-          </Box>
+              </Link>
+            ))}
+          </Grid>
 
-          <Box mt={[4]} sx={{ display: ["none", null, "block"] }}>
-            <Card variant="detailCard">
+          <Box sx={{ display: ["none", null, "block"] }}>
+            <Card variant="infoCard" p={[4]}>
               <Grid columns={["auto 1fr"]}>
                 <Box
                   sx={{
@@ -321,54 +212,45 @@ const Search = () => (
                     height: "150px",
                   }}
                 >
-                  <Image src="/search/lasu_logo.png" />
+                  <Image
+                    alt={`cover photo for lasu`}
+                    src="/search/lasu_logo.png"
+                  />
                 </Box>
                 <Box>
-                  <Text variant="headline" m={[0]}>
+                  <Text variant="headline4" color="darker" m={[0]}>
                     Lagos State University
                   </Text>
-                  <Text variant="subtitle">Lagos, Nigeria</Text>
-                  <Link href="https://lasu.edu.ng/">www.lasu.edu.ng</Link>
+                  <Text variant="largeaLabel">Lagos, Nigeria</Text>
+                  <Text variant="body" color="lighter">
+                    <Link href="https://lasu.edu.ng/" target="blank">
+                      www.lasu.edu.ng
+                    </Link>
+                  </Text>
                 </Box>
               </Grid>
               <Box mt={[3]}>
-                <Text sx={{ color: "steel" }}>Departments</Text>
-                <Text
-                  sx={{
-                    color: "darkBlue",
-                    fontWeight: "bold",
-                    fontSize: 2,
-                    my: 1,
-                  }}
-                >
+                <Text variant="mediumLabel" color="dark200">
+                  Departments
+                </Text>
+                <Text variant="headline6" color="darker">
                   26
                 </Text>
-
                 <hr />
 
-                <Text sx={{ color: "steel" }}>Courses</Text>
-                <Text
-                  sx={{
-                    color: "darkBlue",
-                    fontWeight: "bold",
-                    fontSize: 2,
-                    my: 1,
-                  }}
-                >
+                <Text variant="mediumLabel" color="dark200">
+                  Courses
+                </Text>
+                <Text variant="headline6" color="darker">
                   30
                 </Text>
 
                 <hr />
 
-                <Text>Materials on material pal</Text>
-                <Text
-                  sx={{
-                    color: "darkBlue",
-                    fontWeight: "bold",
-                    fontSize: 2,
-                    my: 1,
-                  }}
-                >
+                <Text variant="mediumLabel" color="dark200">
+                  Materials on material pal
+                </Text>
+                <Text variant="headline6" color="darker">
                   341
                 </Text>
               </Box>
@@ -376,20 +258,32 @@ const Search = () => (
           </Box>
         </Grid>
       </Container>
-    </Box> */}
 
-    <Footer
-      dark
-      sx={{
-        textShadow: "0 1px 2px rgba(0,0,0,0.375)",
-        "h2,span,p,a": { color: "white !important" },
-        svg: {
-          fill: "white",
-          filter: "drop-shadow(0 1px 2px rgba(0,0,0,0.25))",
-        },
-      }}
-    ></Footer>
-  </>
-);
+      <Footer
+        dark
+        sx={{
+          textShadow: "0 1px 2px rgba(0,0,0,0.375)",
+          "h2,span,p,a": { color: "white !important" },
+          svg: {
+            fill: "white",
+            filter: "drop-shadow(0 1px 2px rgba(0,0,0,0.25))",
+          },
+        }}
+      ></Footer>
+    </>
+  );
+};
 
-export default Search;
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts
+  // const res = await fetch("https://.../posts");
+  const materials = myMaterials;
+  const categories = myCategories;
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: { materials, categories },
+  };
+}
+
+export default Materials;
