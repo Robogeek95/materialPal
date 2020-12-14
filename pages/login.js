0303d9870ae8477b41405f34ffd1b8a02b7e09ae
robogeek95 <azeezlukman95@gmail.com>
2020-12-13 20:40:41 -0500

    integrated UI for sign in

--- pages/login.js
+++ pages/login.js
@@ -1,29 +1,30 @@
 import Link from "next/link";
+import { Box, Flex, Grid, Text } from "theme-ui";
 import LoginForm from "../components/LoginForm";
 const LoginPage = () => {
   return (
-    <div className="min-h-screen flex flex-col justify-center bg-gray-200">
-      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="text-center mt-24">
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Log in
-          </h2>
-          <p className="mt-2 text-center text-md text-gray-600">
-            {"Don't have an account? "}
-            <Link href="/signup">
-              <a href="#" className="text-blue-500">
-                Sign Up
-              </a>
-            </Link>
-          </p>
-        </div>
-        <div className="mt-8 bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <LoginForm />
+    <Grid columns={["1fr 1.2fr"]}>
+      <Box p={[4]}>
+        <LoginForm />
+      </Box>
 
-          <Link href="/reset-password">Forgot your password?</Link>
-        </div>
-      </div>
-    </div>
+      <Flex
+        sx={{
+          backgroundImage: ' url("/auth/authbg.png") ',
+          backgroundAttachment: "fixed",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          justifyContent: "center",
+          textAlign: "center",
+          p: [3],
+        }}
+      >
+        <Text mt="6" color="gray100" variant="display2">
+          Let’s help you Experience the best comfort while learning{" "}
+        </Text>
+      </Flex>
+    </Grid>
   );
 };
 export default LoginPage;
