8434590b5a3d01c382046442c54c4b8f13b8b831
robogeek95 <azeezlukman95@gmail.com>
2020-12-15 18:42:32 -0500

    added responsiveness

--- pages/login.js
+++ pages/login.js
@@ -3,14 +3,14 @@ import { Box, Flex, Grid, Text } from "theme-ui";
 import LoginForm from "../components/LoginForm";
 const LoginPage = () => {
   return (
-    <Grid columns={["1fr", null, "1fr 1.2fr"]}>
-      <Box p={[4]}>
+    <Grid columns={["1fr", null, null, "1fr 1.2fr"]}>
+      <Box p={[4, null, 6, 4]}>
         <LoginForm />
       </Box>
 
       <Box
         sx={{
-          display: ["none", null, "flex"],
+          display: ["none", null, null, "flex"],
           backgroundImage: ' url("/auth/authbg.png") ',
           backgroundAttachment: "fixed",
           backgroundPosition: "center",
