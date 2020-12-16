1daffa8a22bc859f4011b33d36f5e0bf252125c8
robogeek95 <azeezlukman95@gmail.com>
2020-12-16 05:03:52 -0500

    fixed responsiveness

--- pages/signup.js
+++ pages/signup.js
@@ -2,13 +2,14 @@ import { Box, Flex, Grid, Text } from "theme-ui";
 import SignUpForm from "../components/SignupForm";
 const SignUpPage = () => {
   return (
-    <Grid columns={["1fr 1.2fr"]}>
+    <Grid columns={["1fr", null, null, "1fr 1.2fr"]}>
       <Box p={[4]}>
         <SignUpForm />
       </Box>
 
       <Flex
         sx={{
+          display: ["none", null, null, "flex"],
           backgroundImage: ' url("/auth/authbg.png") ',
           backgroundAttachment: "fixed",
           backgroundPosition: "center",
